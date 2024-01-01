e6a98454e19d5e9888da6c55f6372d941750dc6f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-01 23:31:08 +0700

    feat: Setup

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,23 +1,50 @@
-plugins {
-    kotlin("jvm") version "1.9.0"
-}
+import fr.xpdustry.toxopid.dsl.mindustryDependencies
+import fr.xpdustry.toxopid.spec.ModMetadata
+import fr.xpdustry.toxopid.spec.ModPlatform
+import fr.xpdustry.toxopid.task.GithubArtifactDownload
 
-group = "kennarddh"
-version = "1.0"
+plugins {
+    kotlin("jvm") version "1.9.10"
+    `maven-publish`
+    id("fr.xpdustry.toxopid") version "3.2.0"
+}
 
 repositories {
+    mavenLocal()
     mavenCentral()
-    maven("https://raw.githubusercontent.com/Zelaux/MindustryRepo/master/repository")
-    maven("https://www.jitpack.io")
+    maven("https://maven.xpdustry.com/releases")
+    maven("https://maven.xpdustry.com/mindustry")
+
+    maven {
+        url = uri("http://23.95.107.12:9999/releases")
+        isAllowInsecureProtocol = true
+    }
 }
 
-val mindustryVersion by extra { "v146" }
-val jabelVersion by extra { "93fde537c7" }
+toxopid {
+    // The version with which your mod/plugin is compiled.
+    // If not set, will compile with v143 by default.
+    compileVersion.set("v146")
+    // The version with which your mod/plugin is tested.
+    // If not set, defaults to the value of compileVersion.
+    runtimeVersion.set("v146")
+
+    // The platforms you target, you can choose DESKTOP, HEADLESS or/and ANDROID.
+    // If not set, will target DESKTOP by default.
+    platforms.add(ModPlatform.HEADLESS)
+}
+
+val metadata = ModMetadata.fromJson(project.file("plugin.json"))
+
+project.group = "kennarddh"
+project.version = metadata.version
 
 dependencies {
-    compileOnly("com.github.Anuken.Arc:arc-core:$mindustryVersion")
-    compileOnly("com.github.Anuken.Mindustry:core:$mindustryVersion")
-    annotationProcessor("com.github.Anuken:jabel:$jabelVersion")
+    mindustryDependencies()
+
+    compileOnly("com.xpdustry:kotlin-runtime:3.1.0-k.1.9.10")
+    compileOnly("kennarddh:genesis-core:1.1.0")
+    compileOnly("kennarddh:genesis-common:1.1.0")
 }
 
 kotlin {
@@ -30,10 +57,78 @@ sourceSets {
     }
 }
 
-tasks.register<Jar>("buildJAR") {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+configurations.runtimeClasspath {
+    exclude(group = "org.jetbrains.kotlin")
+    exclude(group = "org.jetbrains.kotlinx")
+}
 
-    from(sourceSets.main.get().output)
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    from(project.file("plugin.json"))
+tasks {
+    jar {
+        doFirst {
+            val metadataFile = temporaryDir.resolve("plugin.json")
+
+            metadataFile.writeText(metadata.toJson(true))
+
+            from(metadataFile)
+        }
+
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        from(sourceSets.main.get().output)
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    }
+}
+
+val downloadKotlinRuntime =
+    tasks.register<GithubArtifactDownload>("downloadKotlinRuntime") {
+        user.set("xpdustry")
+        repo.set("kotlin-runtime")
+        name.set("kotlin-runtime.jar")
+        version.set("v3.1.0-k.1.9.10")
+    }
+
+val downloadGenesisCore =
+    tasks.register<GithubArtifactDownload>("downloadGenesisCore") {
+        user.set("kennarddh-mindustry")
+        repo.set("genesis")
+        name.set("genesis-core-1.1.0.jar")
+        version.set("v1.1.0")
+    }
+
+val downloadGenesisCommon =
+    tasks.register<GithubArtifactDownload>("downloadGenesisCommon") {
+        user.set("kennarddh-mindustry")
+        repo.set("genesis")
+        name.set("genesis-common-1.1.0.jar")
+        version.set("v1.1.0")
+    }
+
+tasks.runMindustryServer {
+    mods.setFrom(setOf(tasks.jar, downloadKotlinRuntime, downloadGenesisCore, downloadGenesisCommon))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "reposilite"
+            url = uri("http://23.95.107.12:9999/releases")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupId
+            artifactId = metadata.name
+            version = version
+            from(components["java"])
+        }
+    }
+}
+
+tasks.register("getArtifactPath") {
+    doLast { println(tasks.jar.get().archiveFile.get().toString()) }
 }
